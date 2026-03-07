06f1e14095abe71f979619c40abd06dfd5a674cc
romanov.da <dimromgomell@gmail.com>
2026-03-07 18:31:59 +0300

    change script

--- scripts/generate_go.sh
+++ scripts/generate_go.sh
@@ -7,7 +7,7 @@ protoc -I ./proto \
     --go_out=./gen/go \
     --go-grpc_out=./gen/go \
     --go_opt=module=github.com/kirt08-cinema-tickets-project/contracts \
-    --go_grpc_opt=module=github.com/kirt08-cinema-tickets-project/contracts \
+    --go-grpc_opt=module=github.com/kirt08-cinema-tickets-project/contracts \
     ./proto/media.proto
 
 echo "Go generation complete"
